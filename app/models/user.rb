8ad0ff89cad38d16a639e7ed7654528f52d3a2b8
TungNguyenIT <tungnv975@gmail.com>
2018-11-13 09:25:09 +0700

    Chapter 12: Reset Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
-  attr_reader :remember_token, :activation_token
+  attr_reader :remember_token, :activation_token, :reset_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+  
   validates :name, presence: true,
-  length: {maximum: Settings.user.name.length}
+    length: {maximum: Settings.user.name.length}
   validates :email, presence: true,
-  length: {maximum: Settings.user.email.length},
-  format: {with: VALID_EMAIL_REGEX},
-  uniqueness: {case_sensitive: Settings.user.case_sensitive}
+    length: {maximum: Settings.user.email.length},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: Settings.user.case_sensitive}
   validates :password, presence: true,
-  length: {minimum: Settings.user.password.length}
-
+    length: {minimum: Settings.user.password.length}
+  
   has_secure_password
   before_save :downcase_email
   before_create :create_avtivation_digest
@@ -55,6 +55,15 @@ class User < ApplicationRecord
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    @reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
